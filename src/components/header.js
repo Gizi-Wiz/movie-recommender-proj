5dbd5868926ae36149f7bd04d3d1e18e618489ba
JG <jg.hernandez236@gmail.com>
2024-09-21 12:59:30 -0400

    New changes

---
+++ src/components/header.js
@@ -0,0 +1,16 @@
+const Header = () => {
+    return(
+        <header className="header">
+            <div className="header-left">
+                <span clasName="header-brand">MyReact App</span>
+            </div>
+            <div className="header-right">
+                <a href="#">HOME</a>
+                <a href="#">Contact</a>
+                <a href="#">About us</a>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
